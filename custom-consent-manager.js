0b0c348ca2442ccc2a399109cc2e2a7a5f8b9e8f
kn-tl <169078134+kn-tl@users.noreply.github.com>
2025-07-28 11:17:11 +0200

    Update custom-consent-manager.js

--- custom-consent-manager.js
+++ custom-consent-manager.js
@@ -406,7 +406,9 @@ class CustomCookieBanner {
 	createCloseButton() {
 		return `
 			<button class="modal-close" aria-label="Terug naar banner">
-				&lt;
+				<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M15 18L9 12L15 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+				</svg>
 			</button>
 		`;
 	}
